d3902fd87d5fd41ad03d5ede4f52c52b34cf83c0
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-03-02 14:59:41 -0800

    Create SnakeBody.h

---
+++ SnakeBody.h
@@ -0,0 +1,12 @@
+#ifndef SNAKEBODY_H
+#define SNAKEBODY_H
+
+#include <QGraphicsRectItem>
+
+class SnakeBody: public QGraphicsRectItem{
+public:
+    SnakeBody(QGraphicsItem* parent=NULL);
+
+};
+
+#endif // SNAKEBODY_H
