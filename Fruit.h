402e8f27d53d223155f3b9040452b7dfcd1f3c1e
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-03-02 14:59:20 -0800

    Create Fruit.h

---
+++ Fruit.h
@@ -0,0 +1,11 @@
+#ifndef FRUIT_H
+#define FRUIT_H
+
+#include <QGraphicsEllipseItem>
+
+class Fruit: public QGraphicsEllipseItem{
+public:
+    Fruit(QGraphicsItem* parent=NULL);
+};
+
+#endif // FRUIT_H
